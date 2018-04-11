a1c92ce4dfe55e5210f597ea1727fa8a57f42c53
the-wightman <damnos@live.co.uk>
2018-04-12 00:49:50 +0100

    Intial repository commit

---
+++ app/views/includes/footer.blade.php
@@ -0,0 +1,1 @@
+<div id="copyright text-right">An aston university student project.</div>
---
+++ app/views/includes/header.blade.php
@@ -0,0 +1,12 @@
+<div class="navbar">
+    <div class="navbar-inner">
+        <a id="logo" href="/">Single Malt</a>
+        <ul class="nav">
+            <li><a href="/home">Home</a></li>
+            <li><a href="/organise">Organise event</a></li>
+            <li><a href="/userEvents">My events</a></li>
+            <li><a href="/search">Search events</a></li>
+        </ul>
+    </div>
+</div>
+
